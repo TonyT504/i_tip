1696f0339fea5b26391e0bcf9649d708b3dacf5a
MiaLaFleur <mia.lafleur@yahoo.com>
2022-06-13 17:35:35 -0500

    delete irrelevant files

--- public/js/TipCalc.js
+++ public/js/TipCalc.js
@@ -1,4 +1,4 @@
-const User = require('../../models/User');
+// const User = require('../../models');
 
 const newReceiptFormHandler = () => {
     const restName = document.querySelector('#restName').value.trim();
@@ -29,11 +29,11 @@ const newReceiptFormHandler = () => {
 
     const newAmt = ogAmtParsed + tipAmt;
 
-    const userId = User.getCurrentUserId();
+    // const userId = User.getCurrentUserId();
 
    fetch('/api/receipts', {
         method: 'POST',
-        body: ({userId, restName, ogAmtParsed, tipChoiceId, tipAmt, newAmt}),
+        body: ({restName, ogAmtParsed, tipChoiceId, tipAmt, newAmt}),
         headers: { 'Content-Type': 'application/json' },
       }).then(response => console.log(response)).catch(err => {
           console.log(err);
